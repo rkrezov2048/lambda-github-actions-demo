8b26788d0fe54ba4965a44d1167769fb0a5deddb
rkrezov2048 <rkrezov@gmail.com>
2021-02-09 15:11:30 +0100

    initial commit

---
+++ handler.py
@@ -0,0 +1,14 @@
+import json
+import pyjokes
+
+
+def get_joke(event, context):
+    body = {
+        "message": "Greetings from Githun. Your function is deployed by a Github Actions. Enjoy your joke",
+        "joke":pyjokes.get_joke()
+    }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
